eb95193ebcb62ab3fd7f64fa1b5ff1b089a16d6e
lucasberenger <thegreatberenger@gmail.com>
2024-10-17 21:37:04 -0300

    testing ec2

--- buscador/settings.py
+++ buscador/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'bootstrap5',
     'app',
 ]
@@ -41,7 +42,18 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+# CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
+
+# CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
+
+CORS_ALLOWED_ORIGINS = ['localhost', '127.0.0.1', '52.91.112.223']
+
+CSRF_TRUSTED_ORIGINS = ['localhost', '127.0.0.1', '52.91.112.223']
+
 
 ROOT_URLCONF = 'buscador.urls'
 
@@ -124,7 +136,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
